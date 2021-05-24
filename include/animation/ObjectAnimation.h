c84fbdaa1011952db552be635503c3e1d1555690
vis4rd <alagaesia.vrael@gmail.com>
2021-05-24 22:10:35 +0200

    add getters and setters in ObjectAnimation

--- include/animation/ObjectAnimation.h
+++ include/animation/ObjectAnimation.h
@@ -19,6 +19,16 @@ public:
 	inline virtual const sf::Transform &getImage1Frame(const float &current_time) const = 0;
 	inline virtual const sf::Transform &getImage2Frame(const float &current_time) const = 0;
 
+	const sf::Transform &getStartingTransform1() const;
+	const sf::Transform &getStartingTransform2() const;
+	const sf::Transform &getEndingTransform1() const;
+	const sf::Transform &getEndingTransform2() const;
+
+	void setStartingTransform1(const sf::Transform &im1_start);
+	void setStartingTransform2(const sf::Transform &im2_start);
+	void setEndingTransform1(const sf::Transform &im1_end);
+	void setEndingTransform2(const sf::Transform &im2_end);
+
 protected:
 	sf::Transform m_image1Start;//starting position of image1
 	sf::Transform m_image2Start;//starting position of image2
--- src/animation/ObjectAnimation.cpp
+++ src/animation/ObjectAnimation.cpp
@@ -25,3 +25,43 @@ m_image2End(image2end)
 {
 
 }
+
+const sf::Transform &ObjectAnimation::getStartingTransform1() const
+{
+	return m_image1Start;
+}
+
+const sf::Transform &ObjectAnimation::getStartingTransform2() const
+{
+	return m_image2Start;
+}
+
+const sf::Transform &ObjectAnimation::getEndingTransform1() const
+{
+	return m_image1End;
+}
+
+const sf::Transform &ObjectAnimation::getEndingTransform2() const
+{
+	return m_image2End;
+}
+
+void ObjectAnimation::setStartingTransform1(const sf::Transform &im1_start)
+{
+	m_image1Start = im1_start;
+}
+
+void ObjectAnimation::setStartingTransform2(const sf::Transform &im2_start)
+{
+	m_image2Start = im2_start;
+}
+
+void ObjectAnimation::setEndingTransform1(const sf::Transform &im1_end)
+{
+	m_image1End = im1_end;
+}
+
+void ObjectAnimation::setEndingTransform2(const sf::Transform &im2_end)
+{
+	m_image2End = im2_end;
+}
