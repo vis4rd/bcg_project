8f31a660ba1a29ea52a8d7a184e19cf539513d28
vis4rd <alagaesia.vrael@gmail.com>
2021-05-29 22:50:24 +0200

    switch in ObjectAnimation from 2D sf::Transform to 3D em::Matrix4

--- include/animation/ObjectAnimation.h
+++ include/animation/ObjectAnimation.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Animation.h"
+#include "../util/ExtendedMath.h"
 
 class ObjectAnimation : public Animation
 {
@@ -9,31 +10,31 @@ public:
 	explicit ObjectAnimation(
 		const float &animation_length,
 		const float &animation_speed,
-		const sf::Transform &image1start,
-		const sf::Transform &image2start,
-		const sf::Transform &image1end,
-		const sf::Transform &image2end);
+		const em::Matrix4f &image1start,
+		const em::Matrix4f &image2start,
+		const em::Matrix4f &image1end,
+		const em::Matrix4f &image2end);
 
 	virtual ~ObjectAnimation() = default;
 
-	inline virtual const sf::Transform &getImage1Frame(const float &current_time) const = 0;
-	inline virtual const sf::Transform &getImage2Frame(const float &current_time) const = 0;
+	inline virtual const em::Matrix4f &getImage1Frame(const float &current_time) const = 0;
+	inline virtual const em::Matrix4f &getImage2Frame(const float &current_time) const = 0;
 
-	const sf::Transform &getStartingTransform1() const;
-	const sf::Transform &getStartingTransform2() const;
-	const sf::Transform &getEndingTransform1() const;
-	const sf::Transform &getEndingTransform2() const;
+	const em::Matrix4f &getStartingTransform1() const;
+	const em::Matrix4f &getStartingTransform2() const;
+	const em::Matrix4f &getEndingTransform1() const;
+	const em::Matrix4f &getEndingTransform2() const;
 
-	void setStartingTransform1(const sf::Transform &im1_start);
-	void setStartingTransform2(const sf::Transform &im2_start);
-	void setEndingTransform1(const sf::Transform &im1_end);
-	void setEndingTransform2(const sf::Transform &im2_end);
+	void setStartingTransform1(const em::Matrix4f &im1_start);
+	void setStartingTransform2(const em::Matrix4f &im2_start);
+	void setEndingTransform1(const em::Matrix4f &im1_end);
+	void setEndingTransform2(const em::Matrix4f &im2_end);
 
 protected:
-	sf::Transform m_image1Start;//starting position of image1
-	sf::Transform m_image2Start;//starting position of image2
-	sf::Transform m_image1End;	//finishing position of image1
-	sf::Transform m_image2End;	//finishing position of image2
+	em::Matrix4f m_image1Start;	///> starting position of image1
+	em::Matrix4f m_image2Start;	///> starting position of image2
+	em::Matrix4f m_image1End;	///> finishing position of image1
+	em::Matrix4f m_image2End;	///> finishing position of image2
 
 private:
 	
--- src/animation/ObjectAnimation.cpp
+++ src/animation/ObjectAnimation.cpp
@@ -3,19 +3,19 @@
 ObjectAnimation::ObjectAnimation()
 :
 Animation(),
-m_image1Start(sf::Transform()),
-m_image2Start(sf::Transform()),
-m_image1End(sf::Transform()),
-m_image2End(sf::Transform())
+m_image1Start(em::Matrix4f()),
+m_image2Start(em::Matrix4f()),
+m_image1End(em::Matrix4f()),
+m_image2End(em::Matrix4f())
 { }
 
 ObjectAnimation::ObjectAnimation(
 	const float &animation_length,
 	const float &animation_speed,
-	const sf::Transform &image1start,
-	const sf::Transform &image2start,
-	const sf::Transform &image1end,
-	const sf::Transform &image2end)
+	const em::Matrix4f &image1start,
+	const em::Matrix4f &image2start,
+	const em::Matrix4f &image1end,
+	const em::Matrix4f &image2end)
 :
 Animation(animation_length, animation_speed),
 m_image1Start(image1start),
@@ -26,42 +26,42 @@ m_image2End(image2end)
 
 }
 
-const sf::Transform &ObjectAnimation::getStartingTransform1() const
+const em::Matrix4f &ObjectAnimation::getStartingTransform1() const
 {
 	return m_image1Start;
 }
 
-const sf::Transform &ObjectAnimation::getStartingTransform2() const
+const em::Matrix4f &ObjectAnimation::getStartingTransform2() const
 {
 	return m_image2Start;
 }
 
-const sf::Transform &ObjectAnimation::getEndingTransform1() const
+const em::Matrix4f &ObjectAnimation::getEndingTransform1() const
 {
 	return m_image1End;
 }
 
-const sf::Transform &ObjectAnimation::getEndingTransform2() const
+const em::Matrix4f &ObjectAnimation::getEndingTransform2() const
 {
 	return m_image2End;
 }
 
-void ObjectAnimation::setStartingTransform1(const sf::Transform &im1_start)
+void ObjectAnimation::setStartingTransform1(const em::Matrix4f &im1_start)
 {
 	m_image1Start = im1_start;
 }
 
-void ObjectAnimation::setStartingTransform2(const sf::Transform &im2_start)
+void ObjectAnimation::setStartingTransform2(const em::Matrix4f &im2_start)
 {
 	m_image2Start = im2_start;
 }
 
-void ObjectAnimation::setEndingTransform1(const sf::Transform &im1_end)
+void ObjectAnimation::setEndingTransform1(const em::Matrix4f &im1_end)
 {
 	m_image1End = im1_end;
 }
 
-void ObjectAnimation::setEndingTransform2(const sf::Transform &im2_end)
+void ObjectAnimation::setEndingTransform2(const em::Matrix4f &im2_end)
 {
 	m_image2End = im2_end;
 }
