8c6ab6d4d0a758b3f68a5626dabd63030191af02
Kankuro13 <59534813+Karunko13@users.noreply.github.com>
2021-06-04 20:44:55 +0200

    Fix ImageButton.cpp and .h

--- include/gui/ImageButton.h
+++ include/gui/ImageButton.h
@@ -2,7 +2,6 @@
 
 #include "Button.h"
 
-
 class ImageButton : public Button
 {
 public:
@@ -11,19 +10,10 @@ public:
 	ImageButton(const sf::Vector2f &pos, const sf::Vector2f &size, const int number);
 	ImageButton(const ImageButton &copy);
 	ImageButton(ImageButton &&source);
-
 	void render(sf::RenderTarget *target) override;
-
 	void update(sf::Vector2i mousePos, sf::Event &event) override;
 
-	
-	
 private:
-	
-	sf::Sprite spr;
-	
-	sf::Texture texture;
-
-	unsigned short m_dropStatus;
-
+	sf::Sprite m_spr;
+	sf::Texture m_texture;
 };	
--- src/gui/ImageButton.cpp
+++ src/gui/ImageButton.cpp
@@ -1,9 +1,7 @@
 #include "../../include/gui/ImageButton.h"
 
 ImageButton::ImageButton()
-:Button() {
-    
-}
+:Button() {}
 
 ImageButton::ImageButton(const sf::Vector2f &pos, const sf::Vector2f &size,const int number)
     :Button(pos, size)
@@ -13,19 +11,19 @@ ImageButton::ImageButton(const sf::Vector2f &pos, const sf::Vector2f &size,const
         if(number == 1)
         {
 
-        texture.loadFromFile("../res/images/1.png");
+        m_texture.loadFromFile("../res/images/1.png");
         }
         else
         {
-        texture.loadFromFile("../res/images/2.png");    
+        m_texture.loadFromFile("../res/images/2.png");    
         }
 
-        spr.setTexture(texture);
+        m_spr.setTexture(m_texture);
 
-        spr.setPosition(m_shape.getPosition().x, m_shape.getPosition().y);
+        m_spr.setPosition(m_shape.getPosition().x, m_shape.getPosition().y);
 
-        spr.setScale(static_cast<float>((m_shape.getSize().x) / (texture.getSize().x)), 
-                static_cast<float>((m_shape.getSize().y) / (texture .getSize().y) ));
+        m_spr.setScale(static_cast<float>((m_shape.getSize().x) / (m_texture.getSize().x)), 
+                static_cast<float>((m_shape.getSize().y) / (m_texture .getSize().y) ));
                 
     }
 
@@ -43,46 +41,30 @@ Button(source)
 void ImageButton::render(sf::RenderTarget *target)
 {   
     target->draw(m_shape);
-    target->draw(spr);
+    target->draw(m_spr);
 }
 
 void ImageButton::update(sf::Vector2i mousePos, sf::Event &event)
 {
-    
-    if(m_shape.getGlobalBounds().contains(mousePos.x, mousePos.y))
-        {
-            m_state = Button::state::HOVER;
 
-            if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
-            {
-                m_state = Button::state::ACTIVE;
-            }
-        }
-        else
-        {
-            m_state = Button::state::IDLE;
-        }
-        
-        if(m_state == Button::state::ACTIVE)
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left) && m_shape.getGlobalBounds().contains(mousePos.x, mousePos.y ))
         {
             char const * lFilterPatterns[3] = { "*.png", "*.jpg","*.bmp"};
             
-
             char const * selection = tinyfd_openFileDialog("Select file", NULL, 3, lFilterPatterns, NULL, 0 );
             if(selection != nullptr)
             {
 
-            texture.loadFromFile(std::string(selection));
+            m_texture.loadFromFile(std::string(selection));
 
-            spr.setTexture(texture, true);
+            m_spr.setTexture(m_texture, true);
             
-            spr.setPosition(m_shape.getPosition().x, m_shape.getPosition().y);
+            m_spr.setPosition(m_shape.getPosition().x, m_shape.getPosition().y);
             
-            spr.setScale(static_cast<float>((m_shape.getSize().x) / (texture.getSize().x)), 
-                static_cast<float>((m_shape.getSize().y) / (texture .getSize().y) ));
+            m_spr.setScale(static_cast<float>((m_shape.getSize().x) / (m_texture.getSize().x)), 
+                static_cast<float>((m_shape.getSize().y) / (m_texture .getSize().y) ));
             }
           
         }   
         
-
 }
