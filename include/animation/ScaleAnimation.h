0b28805b39ecf15025af9010d1d0a6e790b503d4
Kankuro13 <59534813+Karunko13@users.noreply.github.com>
2021-06-09 22:31:42 +0200

    Fix ScaleAnimation

--- include/animation/ScaleAnimation.h
+++ include/animation/ScaleAnimation.h
@@ -20,14 +20,18 @@ class ScaleAnimation : public ObjectAnimation
 	inline const em::Matrix4f getImage1Frame(const float &current_time) const override
 	{	
 		em::Matrix4f temp = m_image1Start;
-		temp.translate(sf::Vector3<float>(480.f-480.f*current_time/m_totalTime,340.f-340.f*current_time/m_totalTime,0.f));
-		temp.scale(sf::Vector3<float>(1.f * current_time/m_totalTime, 1.f* current_time/m_totalTime, 0.5f));
+		temp.translate(sf::Vector3<float>(0.f, 0.f, 1.f*(current_time/m_totalTime) ));
 		return temp;
 	}
 
 	inline const em::Matrix4f getImage2Frame(const float &current_time) const override
 	{
 		em::Matrix4f temp = m_image2Start;
+		temp.translate(sf::Vector3<float>
+			(480.f-480.f*current_time/m_totalTime,
+			340.f-340.f*current_time/m_totalTime,
+			0.f));
+		temp.scale(sf::Vector3<float>(1.f * current_time/m_totalTime, 1.f* current_time/m_totalTime, 1.f*current_time));
 		return temp;
 	}
 
