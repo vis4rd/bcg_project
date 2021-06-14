e26527eee4658d1ad2b2666dd330d34acad1df17
Kamil-Jagodzinski <KamilJ12345@outlook.com>
2021-06-14 23:56:59 +0200

    documentation

--- include/animation/Animation.h
+++ include/animation/Animation.h
@@ -2,22 +2,62 @@
 
 #include "../root/Root.h"
 
+
+/**
+ * @brief Base class of all animation
+ */
 class Animation
 {
 public:
+	/**
+	 * Default constructor
+	 */
 	Animation();
+	
+	/**
+	 * @brief Contructor settings animation length and speed
+	 *
+	 * @param animation_length The animation length
+	 * @param animation_speed The animation speed
+	 */
 	explicit Animation(const float &animation_length, const float &animation_speed);
+	
+	/**
+	 * @brief Destructor
+	 */
 	virtual ~Animation() = default;
 
+	/**
+	 * @brief Getter of the leangth of animation.
+	 *
+	 * @return The animation length.
+	 */
 	virtual const float &getAnimationLength() const; //in seconds
+	
+	/**
+	 * @brief Getter of the speed of animation.
+	 *
+	 * @return The animation speed.
+	 */
 	virtual const float &getAnimationSpeed() const;
 
+	/**
+	 * @brief Setter of the animation length.
+	 *
+	 * @param seconds time in seconds
+	 */
 	virtual void setAnimationLength(const float &seconds);
+	
+	/**
+	 * @brief Setter of the animation speed.
+	 *
+	 * @param speed_factor new speed value
+	 */
 	virtual void setAnimationSpeed(const float &speed_factor);
 
 protected:
-	float m_totalTime; //in seconds
-	float m_currentTime; // interval [ 0 ; 1 ]
-	float m_speedFactor; // x < 1 : slower, x > 1 faster
+	float m_totalTime; 
+	float m_currentTime; 
+	float m_speedFactor; 
 	
 };
--- include/animation/PixelAnimation.h
+++ include/animation/PixelAnimation.h
@@ -2,37 +2,99 @@
 
 #include "Animation.h"
 
+
+/**
+ * @brief Base class of every animation based on transformations on pixels
+ */
 class PixelAnimation : public Animation
 {
 public:
+	/**
+	 * Default constructor
+	 */
 	PixelAnimation();
-	explicit PixelAnimation(
-		const float &animation_length,
-		const float &animation_speed,
-		std::vector<unsigned char> texture1_pixels, 
-		std::vector<unsigned char> texture2_pixels);
 
-	explicit PixelAnimation(
-		const float &animation_length,
-		const float &animation_speed,
-		const sf::Texture &texture1,
-		const sf::Texture &texture2);
+	/**
+	 * @brief Constructor settings parameters of animation
+	 *
+	 * @param animation_length 	Length of animation
+	 * @param animation_speed 	Speed of animation
+	 * @param texture1_pixels 	Texture of first image
+	 * @param texture2_pixels 	Texture of second image
+	 */
+	 explicit PixelAnimation(
+        const float &animation_length,
+        const float &animation_speed,
+        std::vector<unsigned char> texture1_pixels, 
+        std::vector<unsigned char> texture2_pixels);
 
+	/**
+	 * @brief Constructor settings parameters of animation
+	 *
+	 * @param animation_length 	Length of animation
+	 * @param animation_length 	Speed of animation
+	 * @param texture1_pixels 	Texture of first image
+	 * @param texture2_pixels 	Texture of second image
+	 */
+	  explicit PixelAnimation(
+        const float &animation_length,
+        const float &animation_speed,
+        const sf::Texture &texture1,
+        const sf::Texture &texture2);
+
+	/**
+	 * @brief Destructor.
+	 */
 	virtual ~PixelAnimation() = default;
 
+	/**
+	 * @brief 		Calculations of proper pixel sequnces depending from given time
+	 *
+	 * @param 		current_time The current time
+	 *
+	 * @return 		frame that first image adopts
+	 */
 	inline virtual const std::vector<unsigned char> getTexture1Frame(const float &current_time) const = 0;
+	
+	/**
+	 * @brief 		Calculations of proper pixel sequnces depending from given time
+	 *
+	 * @param 		current_time The current time
+	 *
+	 * @return 		frame that second image adopts
+	 */
 	inline virtual const std::vector<unsigned char> getTexture2Frame(const float &current_time) const = 0;
 	
+	/**
+	 * @brief 		Getter of the pixels of first image.
+	 *
+	 * @return 		vector of pixels of first image.
+	 */
 	std::vector<unsigned char> getPixels1() const;
+	
+	/**
+	 * @brief 		Getter of the pixels of second image.
+	 *
+	 * @return 		vector of pixels of second image.
+	 */
 	std::vector<unsigned char> getPixels2() const;
 
+	/**
+	 * @brief 		Setter of the first image.
+	 *
+	 * @param texture1_pixels 	vector of pixels
+	 */
 	void setPixels1(std::vector<unsigned char> texture1_pixels);
+	
+	/**
+	 * @brief 		Setter of the second image.
+	 *
+	 * @param texture1_pixels 	vector of pixels
+	 */
 	void setPixels2(std::vector<unsigned char> texture2_pixels);
 
-protected:
+	protected:
 	std::vector<unsigned char> m_pixels1;
 	std::vector<unsigned char> m_pixels2;
-
-private:
 	
 };
