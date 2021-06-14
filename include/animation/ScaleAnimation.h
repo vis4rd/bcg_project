85e93e0903dcd62cbcf4a9b615a172ddee335e55
Kankuro13 <59534813+Karunko13@users.noreply.github.com>
2021-06-14 13:38:34 +0200

    ScaleAnimation documentation

--- include/animation/ScaleAnimation.h
+++ include/animation/ScaleAnimation.h
@@ -1,12 +1,27 @@
 #pragma once
 
 #include "ObjectAnimation.h"
-
+/**
+ * @brief      Class performing animations for scaling second image to full size
+ * 			   The second picture covers the first
+ */
 class ScaleAnimation : public ObjectAnimation
 {
 	public:
-
+	/**
+	 * Default constructor
+	 */
 	ScaleAnimation();
+	/**
+	 * @brief      Constructor setting animation startup parameters
+	 *
+	 * @param[in]  animation_length  The animation length
+	 * @param[in]  animation_speed   The animation speed
+	 * @param[in]  image1start       The image 1 start position
+	 * @param[in]  image2start       The image 2 start position
+	 * @param[in]  image1end         The image 1 end position 
+	 * @param[in]  image2end         The image 2 end position
+	 */
 	explicit ScaleAnimation(
 		const float &animation_length,
 		const float &animation_speed,
@@ -14,9 +29,17 @@ class ScaleAnimation : public ObjectAnimation
 		const em::Matrix4f &image2start,
 		const em::Matrix4f &image1end,
 		const em::Matrix4f &image2end);
-
+	/**
+	 * @brief      Default destructor
+	 */
 	~ScaleAnimation() = default;
-
+	/**
+	 * @brief      Performs transformations on the first image
+	 *
+	 * @param[in]  current_time  The current time animation
+	 *
+	 * @return     The image 1 frame.
+	 */
 	inline const em::Matrix4f getImage1Frame(const float &current_time) const override
 	{	
 		em::Matrix4f temp = m_image1Start;
@@ -24,6 +47,13 @@ class ScaleAnimation : public ObjectAnimation
 		return temp;
 	}
 
+	/**
+	 * @brief      Performs transformations on the second image
+	 *
+	 * @param[in]  current_time  The current time of animation
+	 *
+	 * @return     The image 2 frame.
+	 */
 	inline const em::Matrix4f getImage2Frame(const float &current_time) const override
 	{
 		em::Matrix4f temp = m_image2Start;
