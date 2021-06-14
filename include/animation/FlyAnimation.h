13c12b772f66ec9486334310e2f00f22f0f75718
Kankuro13 <59534813+Karunko13@users.noreply.github.com>
2021-06-14 13:54:55 +0200

    FlyAnimation documentation

--- include/animation/FlyAnimation.h
+++ include/animation/FlyAnimation.h
@@ -1,12 +1,28 @@
 #pragma once
 
 #include "ObjectAnimation.h"
-
+/**
+ * @brief      Class performing "fly away" animations, 
+ * 			   in the first half of the time first image flies off the screen
+ * 			   in the second half of the time it returns as the second image
+ */	
 class FlyAnimation : public ObjectAnimation
 {
 	public:
-
+	/**
+	 * default constructor
+	 */
 	FlyAnimation();
+	/**
+	 * @brief      Constructor setting animation startup parameters
+	 *
+	 * @param[in]  animation_length  The animation length
+	 * @param[in]  animation_speed   The animation speed
+	 * @param[in]  image1start       The image 1 start position
+	 * @param[in]  image2start       The image 2 start position
+	 * @param[in]  image1end         The image 1 end position 
+	 * @param[in]  image2end         The image 2 end position
+	 */
 	explicit FlyAnimation(
 		const float &animation_length,
 		const float &animation_speed,
@@ -14,9 +30,17 @@ class FlyAnimation : public ObjectAnimation
 		const em::Matrix4f &image2start,
 		const em::Matrix4f &image1end,
 		const em::Matrix4f &image2end);
-
+	/**
+	 * @brief      default destructor
+	 */
 	~FlyAnimation() = default;
-
+	/**
+	 * @brief      Performs transformations on the first image
+	 *
+	 * @param[in]  current_time  The current time animation
+	 *
+	 * @return     The image 1 frame.
+	 */
 	inline const em::Matrix4f getImage1Frame(const float &current_time) const override
 	{	
 		em::Matrix4f temp = m_image1Start;
@@ -33,7 +57,13 @@ class FlyAnimation : public ObjectAnimation
 		
 		return temp;
 	}
-
+	/**
+	 * @brief      Performs transformations on the second image
+	 *
+	 * @param[in]  current_time  The current time of animation
+	 *
+	 * @return     The image 2 frame.
+	 */
 	inline const em::Matrix4f getImage2Frame(const float &current_time) const override
 	{
 		em::Matrix4f temp = m_image2Start;
