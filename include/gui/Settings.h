e002cc4cd337918a0f21733c65d330c261eee6c5
Kankuro13 <59534813+Karunko13@users.noreply.github.com>
2021-06-14 23:13:50 +0200

    Settings documentation

--- include/gui/Settings.h
+++ include/gui/Settings.h
@@ -1,25 +1,71 @@
 #pragma once
 #include "../animation/Animation.h"
-
+/**
+ * @brief      Singleton class, responsible for storing the settings of the entire program
+ * 			   All constructors are deleted to prevent the creation of new objects of this type
+ * 			   Move and copy constructors are also deleted
+ */
 class Settings
 {
 public:	
-
+	/**
+	 * @brief      Getter for the instance.
+	 *
+	 * @return     The instance.
+	 */
 	static Settings * getInstance();
-
+	
 	Settings(Settings& oth) = delete;
 	Settings(Settings&& oth) = delete;
 	void operator=(const Settings &) = delete;
 	void operator=(Settings &&) = delete;
-	
+	/**
+	 * @brief      The path to the first image loaded to program.
+	 *
+	 * @return     m_path1.
+	 */
 	std::string getPath1() const;
+	/**
+	 * @brief      The path to the second image loaded to program
+	 *
+	 * @return     m_path2.
+	 */
 	std::string getPath2() const;
+	/**
+	 * @brief      Gets the current animation.
+	 *
+	 * @return     m_anim.
+	 */
 	unsigned short getCurrentAnim() const;
+	/**
+	 * @brief      Gets the current number of frames.
+	 *
+	 * @return     m_frames.
+	 */
 	unsigned short getCurrentFrames() const;
-
+	/**
+	 * @brief      Sets m_path1.
+	 *
+	 * @param[in]  path1  The path 1
+	 */
 	void setPath1(const std::string &path1);
+	/**
+	 * @brief      Sets m_path2.
+	 *
+	 * @param[in]  path2  The path 2
+	 */
 	void setPath2(const std::string &path2);
+	/**
+	 * @brief      Sets the current animation.
+	 *
+	 * @param[in]  anim  The animation
+	 */
 	void setCurrentAnim(unsigned short anim);
+	/**
+	 * @brief      Sets the current number of frames.
+	 *
+	 * @param[in]  frames  The frames
+	 */
 	void setCurrentFrames(unsigned short frames);
 
 private:
