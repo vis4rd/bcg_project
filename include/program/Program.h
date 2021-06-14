32cd0bd876b7929298ec888801307e55ce75d175
vis4rd <alagaesia.vrael@gmail.com>
2021-06-14 23:55:49 +0200

    add Program documentation

--- include/program/Program.h
+++ include/program/Program.h
@@ -22,35 +22,100 @@
 #include "../gui/Canvas.h"
 #include "../gui/Settings.h"
 
+/**
+ * @brief Class merging everything from the application into one place
+ */
 class Program
 {
 public:
+	/**
+	 * Constructor starting the application
+	 */
 	Program();
+
+	/**
+	 * @brief Destructor cleaning up after application's termination
+	 */
 	~Program();
 
+	/**
+	 * @brief This method is called at application's end
+	 */
 	void endApp();
+
+	/**
+	 * @brief Sets the animation type from application's settings
+	 *
+	 * @param settings The pointer to Setting's instance
+	 */
 	void setAnimationFromSettings(Settings* settings);
+
+	/**
+	 * @brief Sets the frame count of the animation from application's settings
+	 *
+	 * @param settings The pointer to Setting's instance
+	 */
 	void setFramesFromSettings(Settings* settings);
 	
+	/**
+	 * @brief This member function sets a flag calling Program's update even if there's no event happening at the moment
+	 */
 	static void requestUpdate();
+
+	/**
+	 * @brief This member function expires the request for calling Program's update
+	 */
 	static void expireRequestUpdate();
 
+
+	/**
+	 * @brief Update of delta time
+	 * 
+	 * Delta time is a time measured from drawing previous frame to generating the next one.
+	 * The faster is the computer (more frames per second) the smaller is the delta time.
+	 */
 	void updateDeltaTime();
+
+	/**
+	 * @brief Update all events happening in the application
+	 */
 	void updateSFMLEvents();
+
+	/**
+	 * @brief Gather up all main updates of the application
+	 *
+	 * @param mousePos The mouse position relative to the window
+	 * @param event The event handler 
+	 */
 	void update(sf::Vector2i mousePos, sf::Event &event);
+
+	/**
+	 * @brief Save a sequence of bitmaps from animation into the file on hard drive
+	 *
+	 * @param mousePos The mouse position given to updates of Canvas and Timeline
+	 * @param event The event handler given to updates of Canvas and Timeline
+	 */
 	void saveSequence(sf::Vector2i mousePos, sf::Event &event);
+
+	/**
+	 * @brief Renders everything into the window
+	 */
 	void render();
+
+	/**
+	 * @brief Run method is called by main function
+	 */
 	void run();
 
 private:
-	sf::RenderWindow *m_window;
-	sf::Event m_event;
+	sf::RenderWindow *m_window;///> Target which all object are rendered to
+	sf::Event m_event;///> Event handler
 
-	sf::Clock m_dtClock;
-	float m_deltaTime;
+	sf::Clock m_dtClock;///> Clock measuring delta time between frames
+	float m_deltaTime;///> Time between rendered frames
 
-	PanelTimeline *m_timePanel;
-	PanelButton *m_buttonPanel;
+	PanelTimeline *m_timePanel;///> Panel containing applications contents
+	PanelButton *m_buttonPanel;///> Panel containing applications settings
 
-	static bool requestedUpdate;
+	static bool requestedUpdate;///> Flag declaring whether window should be updated without event or not
 };
